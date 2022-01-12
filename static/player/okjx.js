cea2371a8c2afc1ac67bf97c8926da69d693f3f9
baimirror <18328037528@163.com>
2022-01-12 19:44:44 +0800

    first commit

---
+++ static/player/okjx.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe width="100%" height="100%" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen="true" src="https://www.pangujiexi.com/pangu/?url='+MacPlayer.PlayUrl+'" autoplay="true"></iframe>';
+MacPlayer.Show();
---
+++ static/player/okjxplayer.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe width="100%" height="100%" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen="true" src="https://www.1717yun.com/api/?url='+MacPlayer.PlayUrl+'" autoplay="true"></iframe>';
+MacPlayer.Show();
